9bdc9392c93dd644f3d4d14ea342e5dc313c7fd0
Rohith198-cmd <rohithpalani67@gmail.com>
2025-01-26 11:56:26 +0530

    completed

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-const readFile=require('./readfile')
+const express=require('express')
+const {executeQuery} = require('./table.js')
+const batsmansRoute=require('./ROUTS/batsmansrouts.js')
 
-readFile()
+const app=express()
+const PORT=2125
+
+app.use(express.json())
+
+app.use('/batsmans',batsmansRoute)
+
+
+app.all('*',(req,res)=>{
+    res.status(404).send('404 not found')
+})
+
+app.listen(PORT,(err)=>{
+    if(err){
+        console.log(err);
+        return
+    }
+    console.log(`server is running on http://localhost:${PORT}`);
     
+})
---
+++ table.js
@@ -0,0 +1,14 @@
+const mysql=require('mysql2/promise')
+
+const executeQuery=async(query,data)=>{
+    const connection=await mysql.createConnection({
+        user:'root',
+        password:'@Rohithpalani2001',
+        host:'localhost',
+        database:'sql_practice'
+    })
+    const res=await connection.execute(query,data)
+    return res
+}
+
+module.exports={executeQuery}
